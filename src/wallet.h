62e96539ffdaeaf68d107c430b289e8360a4e705
blockcig <slatedeveloper@gmail.com>
2018-04-24 08:59:41 +0200

    Update minimum wallet version

--- src/wallet.h
+++ src/wallet.h
@@ -68,12 +68,12 @@ class CWalletTx;
 
 /** (client) version numbers for particular wallet features */
 enum WalletFeature {
-    FEATURE_BASE = 10500, // the earliest version new wallets supports (only useful for getinfo's clientversion output)
+    FEATURE_BASE = 9900, // the earliest version new wallets supports (only useful for getinfo's clientversion output)
 
     FEATURE_WALLETCRYPT = 40000, // wallet encryption
     FEATURE_COMPRPUBKEY = 60000, // compressed public keys
 
-    FEATURE_LATEST = 61000
+    FEATURE_LATEST = 9900
 };
 
 enum AvailableCoinsType {
