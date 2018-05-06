eeda6e3ab18e080f0eda0e2864e0998355580514
Mrs-X <Mrs-X@users.noreply.github.com>
2018-05-06 21:53:50 +0200

    [GUI] Fix wrongly displayed balance on Overview tab

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -209,10 +209,11 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
         nWatchOnlyLockedBalance = pwalletMain->GetLockedWatchOnlyBalance();
     }
     // SLX Balance
-    CAmount nTotalBalance = balance + unconfirmedBalance;
-    CAmount slxAvailableBalance = balance - immatureBalance - nLockedBalance;
+    CAmount nTotalBalance = balance + unconfirmedBalance + nLockedBalance;
+    CAmount slxAvailableBalance = balance - immatureBalance;
     CAmount nTotalWatchBalance = watchOnlyBalance + watchUnconfBalance + watchImmatureBalance;    
-    CAmount nUnlockedBalance = nTotalBalance - nLockedBalance;
+    CAmount nUnlockedBalance = nTotalBalance - nLockedBalance - nLockedBalance; // increment nLockedBalance twice because it was added to
+                                                                                // nTotalBalance above
     // zSLX Balance
     CAmount matureZerocoinBalance = zerocoinBalance - unconfirmedZerocoinBalance - immatureZerocoinBalance;
     // Percentages
