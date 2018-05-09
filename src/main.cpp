3defc3bcc1291a957e84db8e5fd43b8e6ca16ac5
blockcig <slatedeveloper@gmail.com>
2018-05-09 23:19:52 +0200

    Correct masternode payment value

--- src/main.cpp
+++ src/main.cpp
@@ -2379,9 +2379,9 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCou
         ret = blockValue * 0.9;
     } else {
         //When zSLX is staked, masternode gets less SLX
-        ret = (blockValue - 27) * COIN;
+        ret = blockValue - 27 * COIN;
         if (isZSLXStake)
-            ret = (blockValue - 30) * COIN;
+            ret = blockValue - 30 * COIN;
     }
 
     return ret;
