6e62027a8ba4f2b07fe5a29b7a538e9d9824c4fc
Mrs-X <Mrs-X@users.noreply.github.com>
2018-05-10 22:15:07 +0200

    [Build] Remove unnecessary BOOST dependency

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -5064,11 +5064,10 @@ void CWallet::ReconsiderZerocoins(std::list<CZerocoinMint>& listMintsRestored, s
 
 string CWallet::GetUniqueWalletBackupName(bool fzslxAuto) const
 {
-    posix_time::ptime timeLocal = posix_time::second_clock::local_time();
     stringstream ssDateTime;
+    std::string strWalletBackupName = strprintf("%s", DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime()));
+    ssDateTime << strWalletBackupName;
 
-
-    ssDateTime << gregorian::to_iso_extended_string(timeLocal.date()) << "-" << timeLocal.time_of_day();
     return strprintf("wallet%s.dat%s", fzslxAuto ? "-autozslxbackup" : "", DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime()));
 }
 
