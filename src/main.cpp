eb5139160a362c8dd3ffc070f8d64e4d12826082
Slate Random Dev <slate@i2pmail.org>
2018-07-02 15:51:26 +0200

    [Qt] Show progress percent for zslx reindex operations

--- src/main.cpp
+++ src/main.cpp
@@ -2498,6 +2498,7 @@ bool RecalculateSLXSupply(int nHeightStart)
 bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError)
 {
     // SLATE: recalculate Accumulator Checkpoints that failed to database properly
+    uiInterface.ShowProgress(_("Calculating missing accumulators..."), 0);
     if (!listMissingCheckpoints.empty() && chainActive.Height() >= Params().Zerocoin_StartHeight()) {
         LogPrintf("%s : finding missing checkpoints\n", __func__);
 
@@ -2507,6 +2508,8 @@ bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError
         // find each checkpoint that is missing
         CBlockIndex* pindex = chainActive[nZerocoinStart];
         while (pindex) {
+            uiInterface.ShowProgress(_("Calculating missing accumulators..."), std::max(1, std::min(99, (int)((double)(pindex->nHeight - nZerocoinStart) / (double)(chainActive.Height() - nZerocoinStart) * 100))));
+
             if (ShutdownRequested())
                 return false;
 
@@ -2537,6 +2540,7 @@ bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError
             }
             pindex = chainActive.Next(pindex);
         }
+        uiInterface.ShowProgress("", 100);
     }
     return true;
 }
