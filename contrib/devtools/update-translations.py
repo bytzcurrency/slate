369c6c144b05e8ddfe0c8e418a908d29522dab25
Gediminas <slatedeveloper@gmail.com>
2018-08-20 13:37:33 +1000

    Update translation script to ensure Python 3 compatibility

--- contrib/devtools/update-translations.py
+++ contrib/devtools/update-translations.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2014 Wladimir J. van der Laan
+# Copyright (c) 2018 The SLATE Core Developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
@@ -15,7 +16,6 @@ It will do the following automatically:
 TODO:
 - auto-add new translations to the build system according to the translation process
 '''
-from __future__ import division, print_function
 import subprocess
 import re
 import sys
@@ -36,12 +36,12 @@ def check_at_repository_root():
     if not os.path.exists('.git'):
         print('No .git directory found')
         print('Execute this script at the root of the repository', file=sys.stderr)
-        exit(1)
+        sys.exit(1)
 
 def fetch_all_translations():
     if subprocess.call([TX, 'pull', '-f', '-a']):
         print('Error while fetching translations', file=sys.stderr)
-        exit(1)
+        sys.exit(1)
 
 def find_format_specifiers(s):
     '''Find all format specifiers in a string.'''
@@ -51,10 +51,7 @@ def find_format_specifiers(s):
         percent = s.find('%', pos)
         if percent < 0:
             break
-        try:
-            specifiers.append(s[percent+1])
-        except:
-            print('Failed to get specifier')
+        specifiers.append(s[percent+1])
         pos = percent+2
     return specifiers
 
